84b385eb70a8f268cc5106d48e80de94c297c24b
Faysal Dalier <583762@edu.rocmn.nl~>
2024-02-28 14:40:48 +0100

    g

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,25 +1,23 @@
 <?php
 
-define('geen_wisselgeld_melding', 'geen wisselgeld');
-
 if ($argc == 1) { 
-    echo geen_wisselgeld_melding; 
+    echo "geen wisselgeld"; 
     exit; 
 }
 
 $geldgeef = intval($argv[1]);
 
 if ($geldgeef <= 0) { 
-    echo geen_wisselgeld_melding; 
-    exit; 
+    echo "geen wisselgeld";
+    exit;
 }
 
-$eenheden = array(50, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05); 
+define('EENHEDEN', [50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05]); 
 
-foreach ($eenheden as $eenheid) {  
-    if ($geldgeef >= $eenheid) {
+foreach (EENHEDEN as $eenheid) {  
+    if ($geldgeef >= $eenheid) { 
         $count = floor($geldgeef / $eenheid); 
-        echo "$count x " . $eenheid . " euro\n"; 
+        echo "$count x " . $eenheid . " euro\n";
         $geldgeef = $geldgeef % $eenheid; 
     }
 }
