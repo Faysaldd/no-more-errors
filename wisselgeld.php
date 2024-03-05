c78a7d48429ea93adf7b81a17ad0786a670cbfa9
Faysal Dalier <583762@edu.rocmn.nl~>
2024-03-05 09:47:28 +0100

    g

--- wisselgeld.php
+++ wisselgeld.php
@@ -2,28 +2,43 @@
 
 define('EENHEDEN', [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5]);
 
-if ($argc == 1) {
-    echo "geen wisselgeld";
-    exit;
+function calcWisselgeld($monie) 
+{
+    return round(floatval($monie) * 20) * 5;
 }
 
-$geldgeef = round(floatval($argv[1]) * 20) * 5;
-
-if ($geldgeef <= 0) {
-    echo "geen wisselgeld";
-    exit;
-}
-
-foreach (EENHEDEN as $eenheid) { 
-    if ($geldgeef >= $eenheid) { 
-        $count = floor($geldgeef / $eenheid); 
-        if ($eenheid >= 100) { 
-            echo "$count x " . $eenheid / 100 . " euro\n"; 
-        } else { 
-            echo "$count x " . $eenheid . " cents\n"; 
+function showWisselgeld($wisselmonie) 
+{
+    foreach (EENHEDEN as $eenheid) {
+        if ($wisselmonie >= $eenheid) {
+            $count = floor($wisselmonie / $eenheid);
+            if ($eenheid >= 100) {
+                echo "$count x " . $eenheid / 100 . " euro\n";
+            } else {
+                echo "$count x " . $eenheid . " cent" . ($eenheid > 1 ? 's' : '') . "\n";
+            }
+            $wisselmonie %= $eenheid;
         }
-        $geldgeef = $geldgeef % $eenheid; 
     }
 }
+
+function main($argc, $argv) 
+{
+    if ($argc == 1) {
+        echo "geen wisselgeld";
+        exit;
+    }
+
+    $geldgeef = calcWisselgeld($argv[1]);
+
+    if ($geldgeef <= 0) {
+        echo "geen wisselgeld";
+        exit;
+    }
+
+    showWisselgeld($geldgeef);
+}
+
+main($argc, $argv);
 
 ?>
