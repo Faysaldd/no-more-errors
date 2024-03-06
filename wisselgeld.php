994ce5eb413e06f8364959583a63599ce66dd731
Faysal Dalier <583762@edu.rocmn.nl~>
2024-03-06 11:17:12 +0100

    g

--- wisselgeld.php
+++ wisselgeld.php
@@ -2,43 +2,64 @@
 
 define('EENHEDEN', [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5]);
 
-function calcWisselgeld($monie) 
-{
-    return round(floatval($monie) * 20) * 5;
+function checkArgumenten($argcount) 
+{ 
+    if ($argcount == 1) {
+            throw new Exception("geen wisselgeld gegeven");
+    }
 }
 
-function showWisselgeld($wisselmonie) 
+function checkNegatief($geldgeef) 
 {
-    foreach (EENHEDEN as $eenheid) {
-        if ($wisselmonie >= $eenheid) {
-            $count = floor($wisselmonie / $eenheid);
+    if ($geldgeef <= 0) {
+        throw new Exception("geen positief bedrag");
+    }
+}
+
+function checkOngeldigeInvoer($geldgeef) 
+{
+    if (!is_numeric($geldgeef)) {
+        throw new Exception("ongeldig (moet een getal zijn)");
+    }
+}
+
+function calcWisselgeld($monie) 
+{
+    $wisselmonie = round(floatval($monie) * 20) * 5;
+    return $wisselmonie;
+}
+
+
+
+function showWisselgeld($geldgeef) 
+{
+    foreach (EENHEDEN as $eenheid) { 
+        if ($geldgeef >= $eenheid) {    
+            $count = floor($geldgeef / $eenheid); 
             if ($eenheid >= 100) {
                 echo "$count x " . $eenheid / 100 . " euro\n";
             } else {
-                echo "$count x " . $eenheid . " cent" . ($eenheid > 1 ? 's' : '') . "\n";
+                echo "$count x " . $eenheid . " cent\n";
             }
-            $wisselmonie %= $eenheid;
+            $geldgeef = $geldgeef % $eenheid;
         }
     }
 }
 
-function main($argc, $argv) 
-{
-    if ($argc == 1) {
-        echo "geen wisselgeld";
-        exit;
-    }
 
-    $geldgeef = calcWisselgeld($argv[1]);
 
-    if ($geldgeef <= 0) {
-        echo "geen wisselgeld";
-        exit;
-    }
+checkArgumenten($argc);
 
+$geldgeef = calcWisselgeld($argv[1]);  
+try {
+    checkNegatief($geldgeef);
+    checkOngeldigeInvoer($geldgeef);
     showWisselgeld($geldgeef);
+} catch (Exception $e) {
+    echo "Fout: " . $e->getMessage();
+    exit;
 }
 
-main($argc, $argv);
+showWisselgeld($geldgeef);
 
 ?>
